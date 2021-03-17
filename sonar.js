8ed449cea82d28f54c7f2260ee107cd1c42a2d30
jlc <javierlarios@gmail.com>
2021-03-17 09:32:32 +0000

    “fix_dos”

--- sonar.js
+++ sonar.js
@@ -1,18 +1,17 @@
-const scanner = require('sonarqube-scanner');
+const scanner = require("sonarqube-scanner")
 
 scanner(
-        {
-        serverUrl : 'SONAR_HOST_URL',
-        token : "SONAR_LOGIN",
-        options: {
-                'sonar.projectName':  'Mflix',
-                'sonar.projectDescription': 'Mflix app in nodejs',
-                'sonar.sources': 'src',
-                'sonar.tests': 'test',
-                'sonar.javascript.coveragePlugin': 'lcov',
-                'sonar.javascript.lcov.reportPaths': 'output/coverage/jest/lcov.info'
-                }
-        },
-        () => process.exit()
+  {
+    serverUrl: "http://sonar-qube-jlarios.northeurope.cloudapp.azure.com",
+    token: "88914e8efd509f185c0035b2ae7f1c84ff5075b5",
+    options: {
+      "sonar.projectName": "Mflix",
+      "sonar.projectDescription": "Mflix app in nodejs",
+      "sonar.sources": "src",
+      "sonar.tests": "test",
+      "sonar.javascript.coveragePlugin": "lcov",
+      "sonar.javascript.lcov.reportPaths": "output/coverage/jest/lcov.info",
+    },
+  },
+  () => process.exit(),
 )
-
